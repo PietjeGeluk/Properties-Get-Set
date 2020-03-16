e7799d484626faa824a751b4d12342a0ac80ee93
PietjeGeluk <62058850+PietjeGeluk@users.noreply.github.com>
2020-03-16 16:51:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name    // property
+            { get; set; }
+            public string Age    // property
+            { get; set; }
+
+        }
+        static void Main(string[] args)
+        {
+            Person x = new Person();
+            x.Name = "Kees";
+            x.Age = "20";
+            Console.WriteLine(x.Name + " " + x.Age);
+
+            Person y = new Person();
+            y.Name = "Jan";
+            y.Age = y.Age + 1;
+            Console.WriteLine(y.Name + " " + y.Age);
+        }
+    }
+}
